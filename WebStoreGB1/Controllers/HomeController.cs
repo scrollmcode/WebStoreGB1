499db0af75751cb35846c95befebc1e2a5be85d1
ScrollmCode <Supermcode@mail.ru>
2020-08-25 23:21:39 +0300

    Добавьте файлы проекта.

---
+++ WebStoreGB1/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebStoreGB1.ViewModels;
+
+namespace WebStoreGB1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private readonly List<EmployeeViewModel> _employees = new List<EmployeeViewModel>
+        {
+            new EmployeeViewModel
+            {
+                Id = 1,
+                FirstName = "Иван",
+                SurName = "Иванов",
+                Patronymic = "Иванович",
+                Age = 22,
+                Position = "Начальник"
+            },
+            new EmployeeViewModel
+            {
+                Id = 2,
+                FirstName = "Владислав",
+                SurName = "Петров",
+                Patronymic = "Иванович",
+                Age = 35,
+                Position = "Программист"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("Employees"); ;
+        }
+
+        public IActionResult Employees()
+        {
+            ViewData["Title"] = _configuration["CustomHelloWorld"];
+            return View(_employees);
+        }
+
+        public IActionResult EmployeeDetails(int id)
+        {
+            //Получаем сотрудника по Id
+            var employee = _employees.FirstOrDefault(t => t.Id == id);
+
+            //Если такого не существует
+            if (employee == null)
+                return NotFound(); // возвращаем результат 404 Not Found
+
+            //Иначе возвращаем сотрудника
+            return View(employee);
+        }
+
+    }
+}
