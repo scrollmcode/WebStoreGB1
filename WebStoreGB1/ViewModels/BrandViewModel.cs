87f89b6776a8504528479fcd049ae329fc2f004d
ScrollmCode <Supermcode@mail.ru>
2020-09-05 20:12:05 +0300

    Brands

---
+++ WebStoreGB1/ViewModels/BrandViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStoreGB1.ViewModels
+{
+    public class BrandViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+
+    }
+}
